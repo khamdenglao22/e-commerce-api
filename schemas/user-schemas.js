9fe6f659ee794369b833e1d6ecf2a354d1228ea0
Tou <bkklove486@gmail.com>
2025-06-12 21:17:19 +0700

    remove required

--- schemas/user-schemas.js
+++ schemas/user-schemas.js
@@ -4,13 +4,13 @@ exports.userCreateSchema = Joi.object({
   fullname: Joi.string().required(),
   username: Joi.string().required(),
   password: Joi.string().required(),
-  role_id: Joi.number().required(),
+  // role_id: Joi.number().required(),
 });
 
 exports.userUpdateSchema = Joi.object({
   fullname: Joi.string().required(),
   username: Joi.string().required(),
-  role_id: Joi.number().required(),
+  // role_id: Joi.number().required(),
 });
 
 exports.userLoginSchema = Joi.object({
