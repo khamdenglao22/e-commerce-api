f84db91dfc0e1436b7f973f48842b8d658a5fe21
Tou <bkklove486@gmail.com>
2025-08-04 20:07:26 +0700

    fetch company data for seller

--- controllers/controllers-cus/company-cus-controller.js
+++ controllers/controllers-cus/company-cus-controller.js
@@ -1,6 +1,11 @@
 const CompanyAccountModel = require("../../models/models-bof/company-account-model");
-const { HTTP_NOT_FOUND, HTTP_SUCCESS, HTTP_BAD_REQUEST } = require("../../utils/http_status");
+const {
+  HTTP_NOT_FOUND,
+  HTTP_SUCCESS,
+  HTTP_BAD_REQUEST,
+} = require("../../utils/http_status");
 const { BASE_MEDIA_URL, BRAND_MEDIA_URL } = require("../../utils/constant");
+const CompanyModel = require("../../models/models-bof/company-model");
 
 exports.findCompanyByCurrency = async (req, res) => {
   const { code_currency } = req.query;
@@ -23,12 +28,32 @@ exports.findCompanyByCurrency = async (req, res) => {
       company.dataValues.image = `${BASE_MEDIA_URL}/600x400.svg`;
     }
 
-
     res.status(HTTP_SUCCESS).json({ status: HTTP_SUCCESS, data: company });
   } catch (error) {
     res.status(HTTP_BAD_REQUEST).json({
       status: HTTP_BAD_REQUEST,
       msg: error.message,
     });
+  }
+};
+
+exports.findAll = async (req, res) => {
+  try {
+    await CompanyModel.findAll()
+      .then((response) => {
+        res.status(HTTP_SUCCESS).json({
+          status: HTTP_SUCCESS,
+          data: response,
+        });
+      })
+      .catch((err) => {
+        res
+          .status(HTTP_BAD_REQUEST)
+          .json({ status: HTTP_BAD_REQUEST, msg: err.message });
+      });
+  } catch (error) {
+    res
+      .status(HTTP_BAD_REQUEST)
+      .json({ status: HTTP_BAD_REQUEST, msg: error.message });
   }
 };
